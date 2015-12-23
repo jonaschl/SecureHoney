d92345898edb45ddb6c33a087ec1516f289458ba
jonaschl <jonnyschlag@gmail.com>
2015-12-23 18:10:44 +0100

    clean after installation

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ mkdir -p /opt/securehoney/keys
 mkdir -p /opt/securehoney/logs
 ssh-keygen -t rsa -N '' -f /opt/securehoney/keys/honeykey
 make
+make clean
 cp sshpot /opt/securehoney/sshpot
 chmod +x /opt/securehoney/sshpot
