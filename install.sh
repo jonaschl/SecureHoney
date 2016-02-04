c143313c521ac6e80511035142d16fc53c0f4346
jonaschl <jonnyschlag@gmail.com>
2016-02-04 17:18:46 +0100

    generate key only if no key exist

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 mkdir -p /opt/securehoney/keys
 mkdir -p /opt/securehoney/logs
-ssh-keygen -t rsa -N '' -f /opt/securehoney/keys/honeykey
+if [ -f /opt/securehoney/keys/honeykey ]; then
+  echo -e "\033[32mKey found\033[0m"
+else
+  echo -e "\033[31mNo Key foudn, we will generate a key\033[0m"
+  ssh-keygen -t rsa -N '' -f /opt/securehoney/keys/honeykey
+fi
+echo -e "\033[32mCompile sshpot\033[0m"
 make
 make clean
 cp sshpot /opt/securehoney/sshpot
